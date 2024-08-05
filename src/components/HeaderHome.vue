d731da64850307422a22fc38b8b5568fb8a98a4a
Nemo D'ACREMONT <nemo.d'acremont@enseirb-matmeca.fr>
2024-08-05 15:06:44 +0200

    refactor: move the header to a component file

---
+++ src/components/HeaderHome.vue
@@ -0,0 +1,49 @@
+<template>
+  <header>
+    <h1>eirb.fr</h1>
+    <h2>Services, clubs & associations de l'ENSEIRB-MATMECA</h2>
+
+    <div class="triangle-wrapper">
+      <div class="triangle-right"></div>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+header {
+  padding-top: 20px;
+  background-color: var(--primary-color);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+
+  text-align: center;
+
+  h1 {
+    font-size: 42px;
+    background-color: #fff;
+    color: var(--primary-color);
+    width: fit-content;
+    padding: 8px 16px;
+  }
+
+  h2 { color: #fff; }
+
+  h1, h2 { margin: 0; }
+}
+
+@media screen and (max-width: 794px) {
+  header h2 {
+    width: 340px;
+    padding-bottom: 20px;
+  }
+}
+
+@media screen and (max-width: 370px) {
+  header h2 {
+    width: 250px;
+  }
+}
+</style>
