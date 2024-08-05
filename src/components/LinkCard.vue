8c214d9b2d2a574bb200304dfabcb5590e676001
Nemo D'ACREMONT <nemo.d'acremont@enseirb-matmeca.fr>
2024-08-05 12:25:51 +0200

    refactor: move link card to its own component file

---
+++ src/components/LinkCard.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="card">
+    <img :src="'img/' + link.icon" />
+
+    <h4>{{ link.name }}</h4>
+    <p>{{ link.description }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type  { Link } from '@/assets/links/index.ts';
+
+const props = defineProps<{
+  link: Link
+}>();
+</script>
+
+<style scoped lang="scss">
+.card {
+  margin: 10px;
+  padding: 10px;
+  border-radius: 10px;
+  box-sizing: border-box;
+
+  display: grid;
+  grid-gap: 0 10px;
+  grid-template-columns: 64px auto;
+  grid-template-rows: auto auto;
+
+  background-color: var(--card-bkg-color);
+  color: var(--text-color);
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+
+  transition:
+    transform 0.2s ease-in-out,
+    box-shadow 0.2s ease-in-out;
+
+  &:hover {
+    box-shadow: -4px 4px 10px rgba(0, 0, 0, 0.4);
+    transform: translate(4px, -4px);
+  }
+
+  img {
+    grid-column: 1;
+    grid-row: 1/3;
+
+    width: 64px;
+    height: 64px;
+  }
+
+  h4 {
+    grid-column: 2;
+    grid-row: 1;
+
+    margin: 0;
+    font-size: 23px;
+    color: #3498db;
+    text-decoration: underline;
+  }
+
+  p {
+    grid-column: 2;
+    grid-row: 2;
+
+    margin: 0;
+  }
+}
+</style>
