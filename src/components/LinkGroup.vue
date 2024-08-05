71dfb2495ad7efb64f7c1c3c481a2fc060b8a8bb
Nemo D'ACREMONT <nemo.d'acremont@enseirb-matmeca.fr>
2024-08-05 13:15:27 +0200

    reformat: move LinkGroup vue component to its own file

---
+++ src/components/LinkGroup.vue
@@ -0,0 +1,45 @@
+<template>
+  <h3>{{ linkGroup.name }}</h3>
+
+  <TransitionGroup
+    tag="div"
+    :css="false"
+    @before-enter="onBeforeEnterFn"
+    @enter="onEnterFn"
+    @leave="onLeaveFn"
+    class="cards"
+  >
+    <a v-for="link in linkGroup.links" :href="link.url" :key="link.url" rel="nofollow">
+      <LinkCard :link="link" />
+    </a>
+  </TransitionGroup>
+</template>
+
+<script setup lang="ts">
+import type { LinkGroup } from '@/assets/links/index.ts';
+import LinkCard from '@/components/LinkCard.vue';
+import { onBeforeEnterFn, onEnterFn, onLeaveFn } from '@/assets/animations.ts';
+
+const props = defineProps<{
+  linkGroup: LinkGroup;
+}>();
+</script>
+
+<style lang="scss">
+.cards {
+  display: grid;
+  grid-template-columns: repeat(3, minmax(380px, 1fr));
+
+  @media screen and (max-width: 1180px) {
+    grid-template-columns: repeat(2, minmax(380px, 1fr));
+  }
+
+  @media screen and (max-width: 800px) {
+    grid-template-columns: 1fr;
+  }
+
+  a {
+    text-decoration: none;
+  }
+}
+</style>
