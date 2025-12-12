379c28c3fee0ba5fed43f9086e15ccea93412a41
DONIAS Arno <ardonias@bordeaux-inp.fr>
2025-12-12 13:38:53 +0100

    move gala

--- src/assets/links/assos.ts
+++ src/assets/links/assos.ts
@@ -125,12 +125,6 @@ const assos: LinkGroup = {
       },
     },
     {
-      name: "Gala Mos'fête",
-      description: "Équipe d'organisation du gala",
-      url: 'https://gala.eirb.fr/',
-      icon: 'associations/x128/gala.png',
-    },
-    {
       name: 'F6KQH',
       description: 'Club radioamateur',
       url: 'https://f6kqh.enseirb-matmeca.fr/',
